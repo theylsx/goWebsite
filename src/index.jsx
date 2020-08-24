c4501ba1cbe5c44d033a438a248e011bfc546d9e
chen887 <1830882822@qq.com>
2020-08-25 00:46:02 +0800

    网站首页动态渲染

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App.jsx'
 import User from './utils/memoryUtil'
 import Store from './utils/storeUtil'
 
 User.user = Store.getUser()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
--- src/pages/homePage/home-page.jsx
+++ src/pages/homePage/home-page.jsx
@@ -1,86 +1,90 @@
-import React, { Component } from "react";
-import { Layout, Menu, Button, Carousel, List, Divider } from "antd";
-import { createBrowserHistory } from "history";
+import React from "react";
+import {Carousel, List, Divider} from "antd";
 
 import "./home-page.css";
-
-const { Header, Content, Footer } = Layout;
-const history = createBrowserHistory();
-
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires.",
-];
+import ajax from "../../utils/ajaxUtil";
+import swiper1 from "./../../img/1.jpg"
+import swiper2 from "./../../img/2.jpg"
+import swiper3 from "./../../img/3.jpg"
 
 export default class HomePage extends React.Component {
-  toLogin = () => {
-    history.push("/login");
-    history.go();
-  };
 
-  render() {
-    return (
-      // <Layout>
-      //     <Header className="home_header">
-      //         <div className="home_logo">医路随学</div>
-      //         <div className="menu_container">
-      //             <Menu mode="horizontal">
-      //                 <Menu.Item key="1">课堂纪实</Menu.Item>
-      //                 <Menu.Item key="2">主题活动</Menu.Item>
-      //                 <Menu.Item key="3">医科普</Menu.Item>
-      //                 <Menu.Item key="4">文创更新</Menu.Item>
-      //                 <Menu.Item key="5">志愿者说</Menu.Item>
-      //             </Menu>
-      //         </div>
-      //         <div className="button_container">
-      //             <Button
-      //                 className="login_button"
-      //                 shape="round"
-      //                 type="primary"
-      //                 onClick={this.toLogin}
-      //                 >登录</Button>
-      //             <Button shape="round" type="primary">注册</Button>
-      //         </div>
+    state = {
+        articleList: []
+    }
 
-      //     </Header>
+    componentDidMount() {
+        ajax("/api/getArticle", {}, "POST").then(response => {
+            const articleList = response.data
+            this.setState({articleList})
+        })
+    }
 
-      <div className="home_content">
-        <Carousel style={{ width: "718px" }} autoplay>
-          <h1 className="h1">1</h1>
-          <h1 className="h1">2</h1>
-          <h1 className="h1">3</h1>
-        </Carousel>
+    render() {
 
-        <div className="list_container">
-          <div style={{ width: "700px", margin: "0 auto" }}>
-            <Divider className="divider" orientation="left">
-              课堂纪实
-            </Divider>
-            <List bordered>
-              {data.map((item, key) => (
-                <List.Item key={key}>{item}</List.Item>
-              ))}
-            </List>
+        const {articleList} = this.state;
 
-            <Divider className="divider" orientation="left">
-              主题活动
-            </Divider>
-            <List></List>
+        return (
+            // <Layout>
+            //     <Header className="home_header">
+            //         <div className="home_logo">医路随学</div>
+            //         <div className="menu_container">
+            //             <Menu mode="horizontal">
+            //                 <Menu.Item key="1">课堂纪实</Menu.Item>
+            //                 <Menu.Item key="2">主题活动</Menu.Item>
+            //                 <Menu.Item key="3">医科普</Menu.Item>
+            //                 <Menu.Item key="4">文创更新</Menu.Item>
+            //                 <Menu.Item key="5">志愿者说</Menu.Item>
+            //             </Menu>
+            //         </div>
+            //         <div className="button_container">
+            //             <Button
+            //                 className="login_button"
+            //                 shape="round"
+            //                 type="primary"
+            //                 onClick={this.toLogin}
+            //                 >登录</Button>
+            //             <Button shape="round" type="primary">注册</Button>
+            //         </div>
 
-            <Divider className="divider" orientation="left">
-              医科普
-            </Divider>
-            <List></List>
-          </div>
-        </div>
-      </div>
+            //     </Header>
 
-      // <Footer style={{ textAlign: "center" }}>Created by goWithU</Footer>
+            <div className="home_content">
+                <Carousel style={{width: "718px"}} autoplay>
+                    <a><img src={swiper1}/></a>
+                    <a><img src={swiper2}/></a>
+                    <a><img src={swiper3}/></a>
+                    {/*{articleList.map((article, index) => <a href={article.link}><img src={article.cover} /></a>)}*/}
+                </Carousel>
 
-      // </Layout>
-    );
-  }
+                <div className="list_container">
+                    <div style={{width: "700px", margin: "0 auto"}}>
+                        <Divider className="divider" orientation="left">
+                            课堂纪实
+                        </Divider>
+                        <List bordered>
+                            {articleList.map((article, index) =>
+                                <div key={index}>
+                                    <List.Item><a href={article.link}>{article.title}</a></List.Item>
+                                </div>)}
+                        </List>
+
+                        <Divider className="divider" orientation="left">
+                            主题活动
+                        </Divider>
+                        <List></List>
+
+                        <Divider className="divider" orientation="left">
+                            医科普
+                        </Divider>
+                        <List></List>
+                    </div>
+                </div>
+            </div>
+
+            // <Footer style={{ textAlign: "center" }}>Created by goWithU</Footer>
+
+            // </Layout>
+        );
+    }
 }
